1b3d4c1c6cb3e6a4ae3dd70b4d46263cbe6f5de6
Nimaiwb <nimaiofficial123@gmail.com>
2023-05-14 12:11:59 -0400

    added code

--- Hacking_tools/macaddresschanger.py
+++ Hacking_tools/macaddresschanger.py
@@ -1,1 +1,10 @@
-import scapy
+import subprocess
+
+def change_mac(interface, new_mac):
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+# Example usage
+change_mac("eth0", "00:11:22:33:44:55")
